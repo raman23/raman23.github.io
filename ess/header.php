88be44155b0f555bfcd667f59bea0bcc88c235d2
Raman <raman@raman-Inspiron-3521>
2016-05-03 09:29:46 +0530

    new ess

--- ess/header.php
+++ ess/header.php
@@ -7,7 +7,9 @@
 		<title>EssPee</title>
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
 		<link rel="stylesheet" type="text/css" href="css/font-awesome.css" />
+		<link rel="stylesheet" type="text/css" href="css/animate.css" />
 		<link rel="stylesheet" type="text/css" href="css/style2.css" />
+		<link rel="stylesheet" type="text/css" href="css/full-slider.css" />
 	</head>
 <body>
 	<section id="navigation">
@@ -19,16 +21,16 @@
 			        <span class="icon-bar"></span>
 			        <span class="icon-bar"></span>
 			     </button>
-				<a class="navbar-brand" style=" padding: 0px; padding-top: 10px;" href="#"><img src='img/sp.png' class='img-responsive' style='max-height:50px;'></a>
+				<a class="navbar-brand" style=" padding: 0px; padding-top: 10px;" href="index.html"><img src='img/logoblue.png' class='img-responsive' style='max-height:50px;'></a>
 			</div>
 			<div class="collapse navbar-collapse" id="nav">
 		      <ul class="nav navbar-nav navbar-right">
-				<li class='nav-link'><a href="index.php">Home</a></li>
-			    <li class='nav-link'><a href="services.php">Services</a></li>
-			    <li class='nav-link'><a href="carear.php">carear</a></li>
-			    <li class='nav-link'><a href="about.php">About</a></li>
-			    <li class='nav-link'><a href="team.php">Team</a></li>
-			    <li class='nav-link'><a href="contact.php">Contact</a></li>
+				<li class='nav-link'><a class="navig" href="home.html">Home</a></li>
+			    <li class='nav-link'><a class="navig"  href="services.html">Services</a></li>
+			    <li class='nav-link'><a class="navig"  href="carear.html">career</a></li>
+			    <li class='nav-link'><a class="navig"  href="about.html">About</a></li>
+			    <li class='nav-link'><a class="navig"  href="team.html">Team</a></li>
+			    <li class='nav-link'><a class="navig"  href="contact.html">Contact</a></li>
 			</ul>
 		  </div>
 	</div>
---
+++ ess/header2.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<title>EssPee</title>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+		<link rel="stylesheet" type="text/css" href="css/font-awesome.css" />
+		<link rel="stylesheet" type="text/css" href="css/animate.css" />
+		<link rel="stylesheet" type="text/css" href="css/style2.css" />
+		<link rel="stylesheet" type="text/css" href="css/full-slider.css" />
+	</head>
+<body>
+	<section id="navigation">
+		<nav class="navbar navbar-fixed-top navbar-default" >
+		<div class="container">
+			<div class="navbar-header" style='min-height:65px;'>
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#nav">
+					<span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			     </button>
+				<a class="navbar-brand" style=" padding: 0px; padding-top: 10px;" href="index.php"><img src='img/logoblue.png' class='img-responsive' style='max-height:50px;'></a>
+			</div>
+			<div class="collapse navbar-collapse" id="nav">
+		      <ul class="nav navbar-nav navbar-right">
+				<li class='nav-link'><a href="manufacturing.html">Home</a></li>
+			    <li class='nav-link'><a href="services1.html">Services</a></li>
+			    <li class='nav-link'><a href="product-single.html">Products</a></li>
+			    <li class='nav-link'><a href="about1.html">About</a></li>
+			    <li class='nav-link'><a href="contact1.html">Contact</a></li>
+			</ul>
+		  </div>
+	</div>
+	</nav>
+</section>
+<header>
+
+</header>
+
